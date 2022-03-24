239306348a8d87719d93ae3abcd33ef8b37cb787
rodrifernan <rodrifernandez8@gmail.com>
2022-03-24 12:59:33 -0300

    Reordeno carpetas

---
+++ api/src/models/Temperamento.js
@@ -0,0 +1,12 @@
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('temperamento', {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  };
+  
